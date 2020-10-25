f1244c1dcdd58cd54ff32f763d45264b4f2f62c1
comcy <christian.silfang@gmail.com>
2020-10-25 23:00:12 +0100

    started working pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { DatenschutzComponent } from './components/datenschutz/datenschutz.component';
+import { ImpressumComponent } from './components/impressum/impressum.component';
+import { NewsComponent } from './components/news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DatenschutzComponent,
+    ImpressumComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
