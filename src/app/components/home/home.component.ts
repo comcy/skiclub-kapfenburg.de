fd4ac9957da7de2d7f02b28f7e599b2268b6bb60
comcy <christian.silfang@gmail.com>
2020-10-27 22:44:11 +0100

    added impressum, datenschutz, header and content images

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  title = 'Skiclub Kapfenburg e.V.';
+  subtitle = 'Herzlich Willkomen';
+
+  headerImgPath = '/assets/img/header/';
+  contentImgPath = '/assets/img/content/';
+
+  headerImages = ["1.jpeg", "2.jpg", "3.jpg"];
+  contentImages = ["01.jpeg", "02.jpeg", "03.jpeg", "04.jpeg", "05.jpg"];
+
   constructor() { }
 
   ngOnInit(): void {
+
+
+  }
+
+  getHeaderImage(): string {
+    return this.getRandomElement(this.headerImages);
+  }
+
+  getContentImage() {
+    return this.getRandomElement(this.contentImages);
+  }
+
+  private getRandomElement(elements: string[]): string {
+    const random = Math.floor(Math.random() * elements.length);
+    return elements[random];
   }
 
 }
