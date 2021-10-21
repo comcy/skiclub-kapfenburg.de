3fbc0140701c7990ad7164dfe48e51c41dd50824
Silfang, Christian <christian.silfang@zeiss.com>
2021-10-21 22:44:17 +0200

    updated page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,3 +1,18 @@
+/*
+ * Copyright:
+ *
+ * Carl Zeiss Industrielle Messtechnik GmbH
+ * http://www.zeiss.com/imt
+ *
+ * This source code file is part of <<project>>.
+ *
+ * Copyright (c) 2019 - 2021 Carl Zeiss Industrielle Messtechnik GmbH - All Rights Reserved.
+ * ZEISS, ZEISS.com are trademarks of Carl Zeiss AG
+ *
+ * Created on 21. October 2021
+ *
+ */
+
 import { Component, OnInit } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { AppState } from 'src/app/state/app.state';
@@ -28,7 +43,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
 
     this.newsVisibility$.subscribe((visible) => {
-      this.isNewsVisible = visible;
+      this.isNewsVisible = false; // visible; --> disabled NEWS banner
     });
 
     this.images$.subscribe((images) => {
