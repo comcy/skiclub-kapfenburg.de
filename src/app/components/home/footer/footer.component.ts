d642aa1bebba33c4694cba5e1316a2e8eada87a5
Silfang, Christian <christian.silfang@zeiss.com>
2021-10-21 22:44:17 +0200

    updated page

--- src/app/components/home/footer/footer.component.ts
+++ src/app/components/home/footer/footer.component.ts
@@ -1,4 +1,22 @@
+/*
+ * Copyright:
+ *
+ * Skiclub Kapfenburg e.V.
+ * http://www.skiclub-kapfenburg.de
+ *
+ * This source code file is part of skiclub-kapfenburg.de.
+ *
+ * Copyright (c) 2019 - 2021 Christian Silfang (comcy) - All Rights Reserved.
+ *  
+ *
+ * Created on 21. October 2021
+ *
+ */
+
+
+
 import { Component, OnInit } from '@angular/core';
+import { getYear } from 'date-fns'
 
 @Component({
   selector: 'app-footer',
@@ -11,5 +29,9 @@ export class FooterComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+    getYear(): string {
+      return getYear(new Date()).toString();
+    }    
 
 }
