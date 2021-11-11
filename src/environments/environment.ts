5592ba0eaacbf6d35595769c14079e59a6c26dba
Christian Silfang <christian.silfang@gmail.com>
2021-11-12 00:16:22 +0100

    added download link to mobile virew

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,7 @@
+/*
+ * @copyright Copyright (c) 2021 Christian Silfang (comcy) - All Rights Reserved.  
+ */
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
