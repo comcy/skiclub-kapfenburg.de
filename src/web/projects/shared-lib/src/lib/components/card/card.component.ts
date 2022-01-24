1c412eedb3cd34ad818b1b0a950c2987f3703f89
comcy <christian.silfang@gmail.com>
2022-01-24 22:25:21 +0100

    added missing components

---
+++ src/web/projects/shared-lib/src/lib/components/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Card } from './card.interface';
+
+@Component({
+  selector: 'shared-lib-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+})
+export class CardComponent {
+  @Input() card: Card = null;
+
+  constructor() {
+    console.log('>>> ', this.card);
+  }
+}
