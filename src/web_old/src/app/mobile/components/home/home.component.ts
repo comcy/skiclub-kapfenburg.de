97d8c157c987a90325fbf6522815a3b2819524ea
comcy <christian.silfang@gmail.com>
2022-04-23 13:50:11 +0200

    added deprecated folder

---
+++ src/web_old/src/app/mobile/components/home/home.component.ts
@@ -0,0 +1,23 @@
+/*
+ * @copyright Copyright (c) 2021 Christian Silfang (comcy) - All Rights Reserved.  
+ */
+
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  oneDriveLink = 'https://1drv.ms/b/s!AlpybhuWN2nhgeNuHja8yp2t5yNwQw';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
---
+++ src/web_old/src/app/shared/components/header/header.interfaces.ts
@@ -0,0 +1,9 @@
+/*
+ * @copyright Copyright (c) 2021 Christian Silfang (comcy) - All Rights Reserved.  
+ */
+
+
+export class NavigationItem {
+    name: string;
+    route: string;
+}
