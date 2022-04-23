6ee08f91578442e370c6331ef70069543d586721
comcy <christian.silfang@gmail.com>
2022-04-23 13:47:56 +0200

    renamed old web folder and created new one

--- src/web/projects/shared-lib/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of shared-lib
- */
-
-export * from './lib/components/footer';
-export * from './lib/components/header';
-export * from './lib/components/card';
-export * from './lib/shared-lib.module';
--- src/web/src/app/shared/components/header/header.interfaces.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @copyright Copyright (c) 2021 Christian Silfang (comcy) - All Rights Reserved.  
- */
-
-
-export class NavigationItem {
-    name: string;
-    route: string;
-}
