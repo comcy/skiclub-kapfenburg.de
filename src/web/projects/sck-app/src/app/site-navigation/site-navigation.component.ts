da056a7f27242c328a52e03759870c84dae794dd
comcy <christian.silfang@gmail.com>
2022-08-04 21:53:37 +0200

    added angular material

---
+++ src/web/projects/sck-app/src/app/site-navigation/site-navigation.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NavigationItem } from 'projects/shared-lib/src/public-api';
+
+@Component({
+  selector: 'app-site-navigation',
+  templateUrl: './site-navigation.component.html',
+  styleUrls: ['./site-navigation.component.scss'],
+})
+export class SiteNavigationComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  @Input() navItems: NavigationItem[] = [];
+
+  ngOnInit(): void {
+    console.log('>>>> ', this.navItems);
+  }
+
+  routerHome() {
+    this.router.navigateByUrl('');
+  }
+
+  routeTo(routeLink: string) {
+    this.router.navigate([routeLink], { relativeTo: this.activatedRoute });
+  }
+}
