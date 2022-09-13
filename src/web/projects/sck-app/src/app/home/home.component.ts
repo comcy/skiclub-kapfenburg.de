69808a4e0f20ecc97c0dcbbf2cac0b333334ce23
comcy <christian.silfang@gmail.com>
2022-09-13 22:49:25 +0200

    added visioble flag to enable or disable cards

--- src/web/projects/sck-app/src/app/home/home.component.ts
+++ src/web/projects/sck-app/src/app/home/home.component.ts
@@ -22,24 +22,40 @@ export class HomeComponent implements OnInit {
       text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
       actions: [TileActions.Register],
       avatar: 'https://avatars.githubusercontent.com/u/3502336?v=4',
+      visible: true,
     },
     {
       title: '15.01.2023 - Lermoos',
       date: '15.01.2023',
       subTitle: 'Tagesausfahrt',
-      image: '../../../../assets/img/cards/skiboers.jpg',
+      image:
+        'https://cdn.pixabay.com/photo/2014/10/22/18/04/man-498473_960_720.jpg',
       imageDescription: 'sample',
       text: 'orem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
       actions: [TileActions.Share, TileActions.Register],
+      visible: true,
     },
     {
       title: '15.01.2023 - Lermoos',
       date: '15.01.2023',
       subTitle: 'Tagesausfahrt',
-      image: '../../../../assets/img/cards/skiboers.jpg',
+      image:
+        'https://cdn.pixabay.com/photo/2016/11/18/15/40/boy-1835416_960_720.jpg',
       imageDescription: 'sample',
       text: 'orem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.orem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
       actions: [TileActions.Share, TileActions.Register],
+      visible: true,
+    },
+    {
+      title: '15.01.2023 - Lermoos',
+      date: '15.01.2023',
+      subTitle: 'Tagesausfahrt',
+      image:
+        'https://cdn.pixabay.com/photo/2020/01/13/23/15/snowboarding-4763731_960_720.jpg',
+      imageDescription: 'sample',
+      text: 'orem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+      actions: [TileActions.Share, TileActions.Register],
+      visible: true,
     },
     {
       title: '15.01.2023 - Lermoos',
@@ -49,15 +65,7 @@ export class HomeComponent implements OnInit {
       imageDescription: 'sample',
       text: 'orem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
       actions: [TileActions.Share, TileActions.Register],
-    },
-    {
-      title: '15.01.2023 - Lermoos',
-      date: '15.01.2023',
-      subTitle: 'Tagesausfahrt',
-      image: '../../../../assets/img/cards/skiboers.jpg',
-      imageDescription: 'sample',
-      text: 'orem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
-      actions: [TileActions.Share, TileActions.Register],
+      visible: true,
     },
   ];
 
