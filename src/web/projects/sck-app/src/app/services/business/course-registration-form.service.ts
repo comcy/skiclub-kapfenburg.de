471a83faa5a0b650f59271c00f35c292d315c706
comcy <christian.silfang@gmail.com>
2022-09-18 00:01:14 +0200

    added snackbar

--- src/web/projects/sck-app/src/app/services/business/course-registration-form.service.ts
+++ src/web/projects/sck-app/src/app/services/business/course-registration-form.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BaseRegistrationFormServiceInterface } from 'projects/shared-lib/src/lib/components/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CourseRegistrationFormServiceInterface } from 'projects/shared-lib/src/lib/components/forms';
 
 const SHEET_API_URL =
   // 'https://script.google.com/macros/s/AKfycbypcAH77zSIP7REyw2I2mnRUjIdutLquedGuGdlUzPMoIdBAwPBhHCMVq7dkkLpLJQf2w/exec';
@@ -8,18 +9,22 @@ const SHEET_API_URL =
   'https://sheetdb.io/api/v1/md2582mwb9jmk';
 
 @Injectable()
-export class CourseRegistrationFormService extends BaseRegistrationFormServiceInterface {
-  constructor(private http: HttpClient) {
-    super();
-  }
+export class CourseRegistrationFormService
+  implements CourseRegistrationFormServiceInterface
+{
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
 
   /**
    *
    * @param courseRegisterForm
    */
   sendFormToSheetsIo(formData: FormData) {
+    console.log('>>> https://sheetdb.io/api/v1/md2582mwb9jmk');
     this.http.post(SHEET_API_URL, formData).subscribe({
-      next: (response) => console.log(response),
+      next: (response) => {
+        console.log(response);
+        this.snackBar.open('Test', 'Close');
+      },
       error: (error) => console.log(error),
     });
   }
--- src/web/projects/shared-lib/src/lib/components/forms/course-register-form/course-register-form.interfaces.ts
+++ src/web/projects/shared-lib/src/lib/components/forms/course-register-form/course-register-form.interfaces.ts
@@ -1,7 +1,13 @@
+import { Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { BaseFormElements } from '../base/base.interfaces';
 
 export interface CourseRegisterForm {
   courseRegisterForm: FormGroup;
   formFields: BaseFormElements[];
+}
+
+@Injectable()
+export abstract class CourseRegistrationFormServiceInterface {
+  public abstract sendFormToSheetsIo(formData: FormData): void;
 }
