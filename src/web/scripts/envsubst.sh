cf3c6f047176dfe1f70127c08448fcd3e3bc3fcf
comcy <christian.silfang@gmail.com>
2022-09-29 22:57:15 +0200

    Updated scripts

--- src/web/scripts/envsubst.sh
+++ src/web/scripts/envsubst.sh
@@ -1,5 +1,7 @@
-    cd ..
     echo "--------------------------------------------------------------"
+
+    ls -al
+
 
     export COURSE_SHEET_URL=$1
     echo $COURSE_SHEET_URL
@@ -7,8 +9,8 @@
     export TRIP_SHEET_URL=$2
     echo $TRIP_SHEET_URL
 
-    envsubst '${COURSE_SHEET_URL} ${TRIP_SHEET_URL}' < ./projects/sck-app/src/environments/environment.template.ts > ./projects/sck-app/src/environments/environment.prod.ts
+    envsubst '${COURSE_SHEET_URL} ${TRIP_SHEET_URL}' < ../projects/sck-app/src/environments/environment.template.ts > ../projects/sck-app/src/environments/environment.prod.ts
 
     echo "--------------------------------------------------------------"
 
-    cat ./projects/sck-app/src/environments/environment.prod.ts
+    cat ../projects/sck-app/src/environments/environment.prod.ts
