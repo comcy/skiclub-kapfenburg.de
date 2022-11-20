1278735546a19cb57219ff474f82e548dff8ca98
comcy <christian.silfang@gmail.com>
2022-11-20 22:38:59 +0100

    Updated courses-components

--- src/web/projects/courses-lib/src/lib/feature/courses-feature.module.ts
+++ src/web/projects/courses-lib/src/lib/feature/courses-feature.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesPricesComponent } from './courses-prices/courses-prices.component';
+import { CoursesInformationComponent } from './courses-information/courses-information.component';
+import { CoursesRegistrationComponent } from './courses-registration/courses-registration.component';
 
 @NgModule({
-  declarations: [CoursesPricesComponent],
+  declarations: [
+    CoursesPricesComponent,
+    CoursesInformationComponent,
+    CoursesRegistrationComponent,
+  ],
   imports: [CommonModule],
-  exports: [CoursesPricesComponent],
+  exports: [
+    CoursesPricesComponent,
+    CoursesInformationComponent,
+    CoursesRegistrationComponent,
+  ],
 })
 export class CoursesFeatureModule {}
