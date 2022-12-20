5bfa09bc0a0a3c0565377bd584e6d76ec714765b
comcy <christian.silfang@gmail.com>
2022-12-20 18:45:06 +0100

    Make application compile again

--- src/web/projects/sck-app/src/app/trips/components/tabs/registration/registration.component.ts
+++ src/web/projects/sck-app/src/app/trips/components/tabs/registration/registration.component.ts
@@ -1,11 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  TRIP_DATA,
+  BUS_ONLY_PRICE_DATA,
+  BUS_AND_PASS_PRICE_DATA,
+  BOARDING_LIST,
+} from '@data';
+import {
+  Price,
+  Tile,
+  TileActions,
+  Trip,
+} from 'projects/shared-lib/src/lib/models';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
 })
 export class RegistrationComponent implements OnInit {
+  public tripData: Tile[] = TRIP_DATA;
+  public trips: Trip[] = [];
+
+  public busOnlyPrice: Price = BUS_ONLY_PRICE_DATA;
+  public busAndPassPrice: Price[] = BUS_AND_PASS_PRICE_DATA;
+  public boardings: string[] = BOARDING_LIST;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    for (let t of this.tripData) {
+      if (t.actions?.includes(TileActions.Register)) {
+        this.trips.push({
+          destination: t.title,
+          date: t.date,
+          boarding: t.boardings,
+        });
+      }
+    }
+  }
 }
