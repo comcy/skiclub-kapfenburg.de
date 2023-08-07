605e48f996e26629eb96e2763c2645393a244ad2
comcy <christian.silfang@gmail.com>
2023-08-07 22:15:00 +0200

    Added unit test and removed vscode project-settings

--- src/web/projects/gym-lib/src/lib/feature/gym-general-information/gym-general-information.component.spec.ts
+++ src/web/projects/gym-lib/src/lib/feature/gym-general-information/gym-general-information.component.spec.ts
@@ -1,25 +1,68 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  GymInformation,
+  GymInformationCoreServiceInterface,
+} from '../../domain';
 import { GymGeneralInformationComponent } from './gym-general-information.component';
+import { BehaviorSubject, of } from 'rxjs';
 
-describe('GeneralInformationComponent', () => {
+describe('GymGeneralInformationComponent', () => {
   let component: GymGeneralInformationComponent;
   let fixture: ComponentFixture<GymGeneralInformationComponent>;
+  let gymState: GymInformationCoreServiceInterface;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GymGeneralInformationComponent ]
-    })
-    .compileComponents();
+      declarations: [GymGeneralInformationComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GymGeneralInformationComponent);
     component = fixture.componentInstance;
+    gymState = {
+      gymOffers$: new BehaviorSubject<GymInformation[]>([]),
+    };
+    component.gymState = gymState;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a gymState input', () => {
+    expect(component.gymState).toBeDefined();
+  });
+
+  it('should display gym offers', () => {
+    const gymOffers = <GymInformation[]>[
+      {
+        title: 'Title 1',
+        appointment: 'Appointment 1',
+        description: 'Description 1',
+        contact: 'Contact 1',
+      },
+      {
+        title: 'Title 2',
+        appointment: 'Appointment 2',
+        description: 'Description 2',
+        contact: 'Contact 2',
+      },
+    ];
+    gymState.gymOffers$.next(gymOffers);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelectorAll('.info-page').length).toBe(
+      gymOffers.length
+    );
+    gymOffers.forEach((offer, index) => {
+      expect(
+        compiled.querySelectorAll('.info-page')[index].textContent
+      ).toContain(offer.title);
+      expect(
+        compiled.querySelectorAll('.info-page')[index].textContent
+      ).toContain(offer.description);
+    });
   });
 });
