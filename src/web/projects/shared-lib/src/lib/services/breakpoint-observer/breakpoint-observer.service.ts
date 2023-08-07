2b54e9c1e9cfceef9b555288a7b1797dbf77c833
Christian Silfang <christian.silfang@gmail.com>
2023-08-07 21:01:05 +0200

    Fixed observer error and updated dependencies

--- src/web/projects/shared-lib/src/lib/services/breakpoint-observer/breakpoint-observer.service.ts
+++ src/web/projects/shared-lib/src/lib/services/breakpoint-observer/breakpoint-observer.service.ts
@@ -1,21 +1,22 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Injectable } from '@angular/core';
-import { map, Observable, shareReplay } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class BreakpointObserverService {
-  private isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .pipe(
-      map((result) => result.matches),
-      shareReplay()
-    );
+  private isHandset$: Observable<boolean>;
 
   get handsetBreakpoint$(): Observable<boolean> {
     return this.isHandset$;
   }
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.isHandset$ = this.breakpointObserver.observe(Breakpoints.Handset)
+      .pipe(
+        map(result => result.matches)
+      );
+  }
 }
