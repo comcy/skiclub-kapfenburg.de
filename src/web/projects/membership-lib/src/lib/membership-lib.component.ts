f9d7f9cf0c9885b17da28ad59941fae7e5788e27
comcy <christian.silfang@gmail.com>
2023-09-03 15:19:57 +0200

    Migrated angular-material

--- src/web/projects/membership-lib/src/lib/membership-lib.component.ts
+++ src/web/projects/membership-lib/src/lib/membership-lib.component.ts
@@ -2,19 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-membership-lib',
-  template: `
-    <p>
-      membership-lib works!
-    </p>
-  `,
-  styles: [
-  ]
+  template: ` <p>membership-lib works!</p> `,
+  styles: [],
 })
 export class MembershipLibComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
