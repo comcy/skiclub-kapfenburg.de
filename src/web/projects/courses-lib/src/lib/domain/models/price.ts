b4d75277989348aa567fd8f2d32eca23498ecadc
Christian Silfang <christian.silfang@gmail.com>
2023-09-17 09:47:18 +0200

    made pre-commit exec

--- src/web/projects/courses-lib/src/lib/domain/models/price.ts
+++ src/web/projects/courses-lib/src/lib/domain/models/price.ts
@@ -1,43 +1,43 @@
 import { SportType } from './sport-type';
 
 export interface Price {
-  id: string;
-  label: PriceMapping;
-  sportType?: PriceSplitByBySportType[];
-  ageGroup?: PriceSplitByAgeGroup[];
+    id: string;
+    label: PriceMapping;
+    sportType?: PriceSplitByBySportType[];
+    ageGroup?: PriceSplitByAgeGroup[];
 }
 
 export interface PriceSplitByAgeGroup {
-  group: PriceAgeGroup;
-  clubMemberStatus: PriceSplitPerClubMemberStatus[];
+    group: PriceAgeGroup;
+    clubMemberStatus: PriceSplitPerClubMemberStatus[];
 }
 
 export interface PriceSplitPerClubMemberStatus {
-  clubMemberStatus: PriceClubMemberStatus;
-  value: string;
+    clubMemberStatus: PriceClubMemberStatus;
+    value: string;
 }
 
 export interface PriceSplitByBySportType {
-  type: SportType;
-  ageGroup: PriceSplitByAgeGroup[];
+    type: SportType;
+    ageGroup: PriceSplitByAgeGroup[];
 }
 
 export enum PriceMapping {
-  Pass = 'Skipass',
-  BusAndPass = 'Busfahrt und Skipass',
-  Bus = 'Reine Busfahrt (ohne Skipass)',
-  CourseAtHome = 'Kurse im heimischen Gelände',
-  CourseOnTravel = 'Kurse bei den Ausfahrten',
+    Pass = 'Skipass',
+    BusAndPass = 'Busfahrt und Skipass',
+    Bus = 'Reine Busfahrt (ohne Skipass)',
+    CourseAtHome = 'Kurse im heimischen Gelände',
+    CourseOnTravel = 'Kurse bei den Ausfahrten',
 }
 
 export enum PriceClubMemberStatus {
-  Member = 'Mitglieder',
-  All = 'Nichtmitglieder',
+    Member = 'Mitglieder',
+    All = 'Nichtmitglieder',
 }
 
 export enum PriceAgeGroup {
-  All = 'Alle',
-  Adult = 'Erwachsene',
-  Teenager = 'Jugendliche',
-  Child = 'Kinder',
+    All = 'Alle',
+    Adult = 'Erwachsene',
+    Teenager = 'Jugendliche',
+    Child = 'Kinder',
 }
