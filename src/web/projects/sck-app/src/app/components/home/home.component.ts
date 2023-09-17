164fd36a4370f023c3d221e97a1fe9226bca0b49
comcy <christian.silfang@gmail.com>
2023-09-17 10:39:29 +0200

    fixed lints

--- src/web/projects/sck-app/src/app/components/home/home.component.ts
+++ src/web/projects/sck-app/src/app/components/home/home.component.ts
@@ -1,79 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import {
-  Tile,
-  TileActions,
-  TileBehavior,
-  TileStatus,
-} from 'projects/shared-lib/src/lib/models';
-import { PROGRAMM_DOWNLOAD_LINK, PROGRAMM_DOWNLOAD_LINK_SHORT, TRIP_DATA } from '@data';
-import { MarkdownRenderService } from 'projects/shared-lib/src/lib/services';
+import { PROGRAMM_DOWNLOAD_LINK, TRIP_DATA } from '@data';
 import { TripsRegisterDialogComponent } from '@trips-lib';
+import { Tile, TileActions, TileBehavior, TileStatus } from 'projects/shared-lib/src/lib/models';
+import { MarkdownRenderService } from 'projects/shared-lib/src/lib/services';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public title: string = 'Aktuelles';
-  public tileStatusEnum: typeof TileStatus = TileStatus;
-  public tileActionsEnum: typeof TileActions = TileActions;
-  public tileBehaviorEnum: typeof TileBehavior = TileBehavior;
-  public registerLabel: string = 'Anmelden';
-  public tiles: Tile[] = [];
-  public programmDownloadLink = PROGRAMM_DOWNLOAD_LINK;
+    public title: string = 'Aktuelles';
+    public tileStatusEnum: typeof TileStatus = TileStatus;
+    public tileActionsEnum: typeof TileActions = TileActions;
+    public tileBehaviorEnum: typeof TileBehavior = TileBehavior;
+    public registerLabel: string = 'Anmelden';
+    public tiles: Tile[] = [];
+    public programmDownloadLink = PROGRAMM_DOWNLOAD_LINK;
 
-  private trips: Tile[] = TRIP_DATA;
+    private trips: Tile[] = TRIP_DATA;
 
-  constructor(
-    public dialog: MatDialog,
-    public markdown: MarkdownRenderService,
-  ) {}
+    constructor(
+        public dialog: MatDialog,
+        public markdown: MarkdownRenderService,
+    ) {}
 
-  ngOnInit(): void {
-    this.trips.sort((a, b) => {
-      return a.order > b.order // Handle order
-        ? -1
-        : 1 && b.expiration.getTime() - a.expiration.getTime(); // Handle expiration
-    });
+    ngOnInit(): void {
+        this.trips.sort((a, b) => {
+            return a.order > b.order // Handle order
+                ? -1
+                : 1 && b.expiration.getTime() - a.expiration.getTime(); // Handle expiration
+        });
 
-    // then place expired events at the end
-    this.trips.sort((a, b) => {
-      if (a.expiration.getTime() < new Date().getTime()) return 1;
-      else if (b.expiration.getTime() > new Date().getTime()) return -1;
-      else return a.expiration.valueOf() - b.expiration.valueOf();
-    });
+        // then place expired events at the end
+        this.trips.sort((a, b) => {
+            if (a.expiration.getTime() < new Date().getTime()) return 1;
+            else if (b.expiration.getTime() > new Date().getTime()) return -1;
+            else return a.expiration.valueOf() - b.expiration.valueOf();
+        });
 
-    this.trips.map((t) => {
-      t.expired = t.expiration.getTime() < new Date().getTime() ? true : false;
-      t.visible = t.visible === false ? false : true;
-    });
+        this.trips.map((t) => {
+            t.expired = t.expiration.getTime() < new Date().getTime() ? true : false;
+            t.visible = t.visible === false ? false : true;
+        });
 
-    this.tiles = this.trips;
-  }
+        this.tiles = this.trips;
+    }
 
-  public openRegisterDialog(tile: Tile) {
-    let dialogConfig = new MatDialogConfig();
+    public openRegisterDialog(tile: Tile) {
+        const dialogConfig = new MatDialogConfig();
 
-    dialogConfig.autoFocus = true;
-    dialogConfig.data = {
-      tile,
-    };
+        dialogConfig.autoFocus = true;
+        dialogConfig.data = {
+            tile,
+        };
 
-    const dialogRef = this.dialog.open(
-      TripsRegisterDialogComponent,
-      dialogConfig
-    );
+        const dialogRef = this.dialog.open(TripsRegisterDialogComponent, dialogConfig);
 
-    dialogRef.afterClosed().subscribe((result) => {});
-  }
+        dialogRef.afterClosed().subscribe(() => {});
+    }
 
-  public openLink(link: string) {
-    // TODO: Add a snackbar
-    window.open(link, "_blank");
-  }
-
-
+    public openLink(link: string) {
+        // TODO: Add a snackbar
+        window.open(link, '_blank');
+    }
 }
-
