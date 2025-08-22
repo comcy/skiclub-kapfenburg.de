88f65697781e888509084983af001bc67f2576e8
Christian Silfang (comcy) <christian.silfang@gmail.com>
2025-08-22 19:17:17 +0200

    Updated Pilates tile

--- src/web/projects/data/static/gym-pilates.tile.ts
+++ src/web/projects/data/static/gym-pilates.tile.ts
@@ -9,13 +9,15 @@ Pilates ist ein effektives Ganzkörpertraining zur Kräftigung von Bauch, Rücke
 Mit Fokus auf das „Powerhouse“, präzise Übungen und bewusste Atmung verbessert es Haltung und Körpergefühl.
 
 **Termine**
-- 7 x mittwochs 
+- 12 x mittwochs 
+- Start am 24. Sep. 2025
+- Ende am 17. Dez. 2025
 - 8:30 Uhr - 09:30 Uhr 
-- im "Alten Schulhaus" in Hülen
+- im "Alten Schulhaus" in Hülen (Aalener Gasse 12)
 
 **Kursgebühren**
-- Mitglieder: 28,00 € (4 € à Kurseinheit)
-- Nicht-Mtglieder: 42,00 € (6 € à Kurseinheit)
+- Mitglieder: 48,00 € (4 € à Kurseinheit)
+- Nicht-Mtglieder: 72,00 € (6 € à Kurseinheit)
 `;
 
 export const GYM_PILATES_TILE: Tile = {
@@ -23,7 +25,7 @@ export const GYM_PILATES_TILE: Tile = {
     behavior: TileBehavior.View,
     title: 'PILATES',
     type: TileType.Course,
-    date: 'NEU ab Juli 2025',
+    date: '24.9.25 bis 17.12.25',
     subTitle: 'Mittwochs, 8:30 Uhr - 09:30 Uhr (Altes Schulhaus Hülen)',
     image: 'https://cdn.pixabay.com/photo/2016/10/15/18/29/yoga-mat-1743203_960_720.jpg',
     imageDescription: 'pilates',
