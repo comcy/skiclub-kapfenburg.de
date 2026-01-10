2a5c5f1f0658fdf085101f22704f1a845f4b6fb8
Christian Silfang <3502336+comcy@users.noreply.github.com>
2026-01-10 17:24:16 +0100

    skilift-data.ts aktualisieren

--- src/web/projects/data/skilift-data.ts
+++ src/web/projects/data/skilift-data.ts
@@ -10,15 +10,15 @@ import {
 } from 'projects/skilift-lib/src/lib/skilift-info/skilift-info.interfaces';
 
 export const SKILIFT_STATUS: SkiliftSnowStatus = {
-    state: SkiliftState.CLOSED, // OPEN | CLOSED
-    text: 'Skilift NICHT in Betrieb',
-    lastUpdate: '9. Januar 2026 (im Falle von Fragen, nehmt gerne mit uns Kontakt auf.) ',
+    state: SkiliftState.OPEN, // OPEN | CLOSED
+    text: 'Skilift in Betrieb',
+    lastUpdate: '10. Januar 2026 (im Falle von Fragen, nehmt gerne mit uns Kontakt auf.) ',
 };
 
 export const SKILIFT_OPENING_HOURS: SkiliftOpeningHours[] = [
     { day: 'am 8. Januar 2026', time: 'ab 15:00 Uhr' },
     { day: 'am 9. Januar 2026', time: 'geschlossen, zu wenig Schnee' },
-    { day: 'am 10. Januar 2026', time: 'geplant ab 10:00 Uhr' },
+    { day: 'am 10. Januar 2026', time: 'geplant ab 14:00 Uhr' },
     { day: 'am 11. Januar 2026', time: 'geplant ab 10:00 Uhr' },
 ];
 
